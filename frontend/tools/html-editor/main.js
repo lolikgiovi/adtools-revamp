c9768d51b3b61c06e18094a787890a4afd6cf4ab
lolikgiovi <lolik.giovi@gmail.com>
2026-09-05 09:40:44 +0700

    Load Monaco HTML language contribution

--- frontend/tools/html-editor/main.js
+++ frontend/tools/html-editor/main.js
@@ -1,5 +1,6 @@
 import { BaseTool } from "../../core/BaseTool.js";
 import * as monaco from "monaco-editor/esm/vs/editor/editor.api.js";
+import "monaco-editor/esm/vs/basic-languages/html/html.contribution.js";
 import "monaco-editor/esm/vs/language/html/monaco.contribution.js";
 import editorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
 import htmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker";
